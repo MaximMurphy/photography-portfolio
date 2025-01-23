b85f4773a2d8092aec70b8f136b04380b849d476
Maxim Murphy <maxim.murphy8@gmail.com>
2025-01-22 23:05:57 -0500

    Updated CORS in next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
-const S3_BUCKET_URL = "https://35mm-images.s3.us-east-2.amazonaws.com";
-
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [S3_BUCKET_URL || ""], // Add your S3 bucket domain
-    unoptimized: process.env.NODE_ENV === "development", // Optional: disable optimization in development
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "35mm-images.s3.us-east-2.amazonaws.com",
+      },
+    ],
   },
 };
 
